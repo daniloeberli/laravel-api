aee7e11242136eafc5dec795890bdf8887dafaf5
daniloeberli <danilo.eberli@gmail.com>
2023-06-04 11:18:12 +0200

    feat: type API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TypeController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('projects',[UserController::class,'index']);
 Route::get('projects/{id}',[UserController::class,'show']);
+Route::get('types',[TypeController::class,'index']);
+Route::get('types/{slug}',[TypeController::class,'show']);
